4f8a7ee0e6984d3509ee59335e67313a80720f07
SebastianMarquardt <Sebastian.Marquardt99@web.de>
2022-11-13 19:19:49 +0100

    Main page functionality

---
+++ Movies.js
@@ -0,0 +1,42 @@
+fetch("https://swapi.dev/api/films/")
+    .then((response) => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error("NETWORK RESPONSE ERROR");
+        }
+    })
+    .then(data => {
+        console.log(data);
+        displayMovie(data);
+    })
+    .catch((error) => console.error("FETCH ERROR:", error));
+
+function displayMovie(data) {
+    for (let i = 0; i < data.count; i++) {
+        const movie = data.results[i];
+        const movieList = document.getElementById("movieList");
+        const movieName = movie.title;
+        const listItem = document.createElement("li");
+        listItem.name = movieName;
+        movieList.appendChild(listItem);
+
+        const link = document.createElement("a");
+        link.href = "Details#" + (i + 1);
+        link.innerHTML = movieName;
+        listItem.appendChild(link);
+    }
+}
+
+var input = document.getElementById("movieSearch");
+input.onkeyup = function () {
+    var filter = input.value.toUpperCase();
+    var lis = document.getElementsByTagName("li");
+    for (var i = 0; i < lis.length; i++) {
+        var name = lis[i].name.toUpperCase();
+        if (name.includes(filter)) 
+            lis[i].style.display = "list-item";
+        else
+            lis[i].style.display = "none";
+    }
+}
